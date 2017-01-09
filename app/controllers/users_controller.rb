330f1f95836daa8dbe0f2f32940b96a2708cace3
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-01-09 15:57:35 -0500

    Save the changes in the home

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,8 @@ class UsersController < ApplicationController
   load_and_authorize_resource
 
   def show
+    range = (Date.today..6.months.from_now)
+    months = range.to_a.map(&:beginning_of_month).uniq
+    @months = months.map { |date| date.strftime('%Y %b') } 
   end
 end
