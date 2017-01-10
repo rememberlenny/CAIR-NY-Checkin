8b95be0a8009681c11ab339a7384024cfa56cf4a
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-01-09 23:23:41 -0500

    Setup tests for the existing account and the new exist

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,11 @@
 class Account < ActiveRecord::Base
-  def process_phone(phone)
-
+  def self.process_phone(phone)
+    accounts = Account.where(phone: phone)
+    if accounts.count == 0
+      account = Account.new(phone: phone)
+      account.save
+    else
+      account = accounts.first
+    end
   end
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -9,5 +9,28 @@ describe Account, :type => :model do
       expect(account.phone).not_to eq(nil)
     end
   end
+
+  describe "#process_phone" do
+    it "checks for existing account" do
+      phone = FFaker::PhoneNumber.phone_number
+      total = Account.all.count
+      Account.process_phone(phone)
+      new_total = Account.all.count
+      expect(new_total).to eq(total+1)
+    end
+
+    it "creates a new one if it exists" do
+      phone = FFaker::PhoneNumber.phone_number
+      
+      a = Account.new(phone: phone)
+      a.save
+      total = Account.all.count
+
+      Account.process_phone(phone)
+      new_total = Account.all.count
+      expect(new_total).not_to eq(total+1)
+      expect(new_total).to eq(total)
+    end
+  end
 end
 
