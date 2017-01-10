f58c5be1fb734596abb5a0b03f274b748f1ce92d
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-01-10 00:43:50 -0500

    Add the check in the account message

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -4,10 +4,20 @@ RSpec.describe Message, type: :model do
 
  let(:message) {FactoryGirl.build(:message)}
   
-  describe "#create" do
+  describe "#create_on_account" do
     it "setup an account" do
       message.save
       expect(message.message).not_to eq(nil)
     end
+    
+    it "checks for existing account" do
+      phone = FFaker::PhoneNumber.phone_number
+      message = "123"
+      total = Message.all.count
+      Message.create_on_account(phone, message)
+      new_total = Message.all.count
+      expect(new_total).to eq(total + 1)
+    end
   end
+
 end
