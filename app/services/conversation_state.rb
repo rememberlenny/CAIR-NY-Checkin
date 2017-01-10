189964c72f36485ad686fd5a3a92897618fab0e1
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-01-10 18:25:42 -0500

    Save the changes in the canonical

--- app/services/conversation_state.rb
+++ app/services/conversation_state.rb
@@ -1,7 +1,9 @@
 class ConversationState
+  include Rails.application.routes.url_helpers
   def self.get_response(message_trigger)
     if is_reserve_word?(message_trigger)
-      return "Login using this link: "
+      content = "Login using this link: " + Shortener::ShortenedUrl.generate(Rails.application.routes.url_helpers.provider_auth_path("instagram"), host: CANONICAL_HOST).to_s
+      return content
     end
 
     conversation_responses = ConversationResponse.where("lower(trigger) = ?", message_trigger.downcase)
