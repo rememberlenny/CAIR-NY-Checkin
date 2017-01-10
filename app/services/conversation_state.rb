04734687d061bd21043213f11719a296f375fe4b
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-01-10 18:05:44 -0500

    Add the login

--- app/services/conversation_state.rb
+++ app/services/conversation_state.rb
@@ -1,11 +1,20 @@
 class ConversationState
   def self.get_response(message_trigger)
-    conversation_responses = ConversationResponse.where(trigger: message_trigger)
+    if is_reserve_word?(message_trigger)
+      return "Login using this link: "
+    end
+
+    conversation_responses = ConversationResponse.where("lower(trigger) = ?", message_trigger.downcase)
+    
     if conversation_responses.count == 0
       "Sorry. I dont understand."
     else
       conversation_response = conversation_responses.sample
       conversation_response.statement
     end
+  end
+
+  def self.is_reserve_word?(word)
+    ["login"].include? word.downcase
   end
 end
