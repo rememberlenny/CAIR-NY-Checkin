85e250d5b780364f1fb654d6ee89e6e4d2cdb806
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-01-10 14:10:19 -0500

    Save the shortened URL

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   include Concerns::UserImagesConcern
-
+  has_shortened_urls
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :timeoutable, :lockable, :async
