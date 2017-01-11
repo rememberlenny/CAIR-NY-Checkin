9917711dc88a058e678880735d8309b61fd14091
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-01-10 21:56:52 -0500

    Save the changes on the field

--- app/assets/javascripts/scroll-message.js
+++ app/assets/javascripts/scroll-message.js
@@ -3,3 +3,11 @@ $(function() {
   var height = wtf[0].scrollHeight;
   wtf.scrollTop(height);
 });
+
+$('#phone-simulator').on('submit', function(e){
+  e.preventDefault();
+  var field = $('#phone-input');
+  var field_value = field.val();
+  alert(field_value);
+  field.val('');
+});
