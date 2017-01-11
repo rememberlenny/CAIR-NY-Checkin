cc4cc00a769d5d1e29fa22e7c783267ebc0556e3
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-01-10 21:57:20 -0500

    Save the ready

--- app/assets/javascripts/scroll-message.js
+++ app/assets/javascripts/scroll-message.js
@@ -1,13 +1,15 @@
-$(function() {
-  var wtf    = $('.scrollable');
-  var height = wtf[0].scrollHeight;
-  wtf.scrollTop(height);
-});
+$(document).ready(function(){
+  $(function() {
+    var wtf    = $('.scrollable');
+    var height = wtf[0].scrollHeight;
+    wtf.scrollTop(height);
+  });
 
-$('#phone-simulator').on('submit', function(e){
-  e.preventDefault();
-  var field = $('#phone-input');
-  var field_value = field.val();
-  alert(field_value);
-  field.val('');
-});
+  $('#phone-simulator').on('submit', function(e){
+    e.preventDefault();
+    var field = $('#phone-input');
+    var field_value = field.val();
+    alert(field_value);
+    field.val('');
+  });
+})
