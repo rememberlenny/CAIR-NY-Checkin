82016f0518f9771ca16ad4e5ab1958fc45ae7d73
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-01-11 11:49:08 -0500

    Create the working internal view

--- app/assets/javascripts/scroll-message.js
+++ app/assets/javascripts/scroll-message.js
@@ -13,14 +13,25 @@ $(document).ready(function(){
       data: { message: message }
     })
       .done(function( msg ) {
-        console.log( msg );
+        renderResponse(msg, "out");
+        console.log( "Replying with: ", msg );
       });
+  }
+
+  function renderResponse(message, direction){
+    if (direction == "in"){
+      $('#phone-messages').append('<p><b>Prehave:</b> '+ message +'</p>')
+    } else {
+      $('#phone-messages').append('<p><b>You:</b> '+ message +'</p>')
+    }
   }
 
   $('#phone-simulator').on('submit', function(e){
     e.preventDefault();
     var field = $('#phone-input');
     var field_value = field.val();
+    renderResponse(field_value, "in");
+    console.log( "Received: ", field_value );
     submitToNumber(field_value);
     field.val('');
   });
