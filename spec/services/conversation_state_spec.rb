5cb493e38470c0ccac8aa462c7e0a5474d2304f9
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-01-12 18:09:55 -0500

    Get the working tests up and running

--- spec/services/conversation_state_spec.rb
+++ spec/services/conversation_state_spec.rb
@@ -3,15 +3,75 @@ require 'rails_helper'
 RSpec.describe ConversationState do
   describe "catch reserve words" do
     it "if reserve is login" do
+      aa = Account.new(phone: "123")
+      aa.save
+      keyword = "login"
+      cc = ConversationResponse.new(trigger: keyword, statement: "Howdy")
+      cc.save
+      message = Message.new(account_id: aa.id, message: keyword, direction: "out")
+      message.save
+
+      state = ConversationState.get_response(message.message)
+
+      expect(state).not_to eq("Howdy")
     end
+
     it "if reserve is summary" do
+      aa = Account.new(phone: "123")
+      aa.save
+      keyword = "summary"
+      cc = ConversationResponse.new(trigger: keyword, statement: "Howdy")
+      cc.save
+      message = Message.new(account_id: aa.id, message: keyword, direction: "out")
+      message.save
+
+      state = ConversationState.get_response(message.message)
+
+      expect(state).not_to eq("Howdy")
     end
+
     it "if reserve is comments" do
+      aa = Account.new(phone: "123")
+      aa.save
+      keyword = "comments"
+      cc = ConversationResponse.new(trigger: keyword, statement: "Howdy")
+      cc.save
+      message = Message.new(account_id: aa.id, message: keyword, direction: "out")
+      message.save
+
+      state = ConversationState.get_response(message.message)
+
+      expect(state).not_to eq("Howdy")
     end
+
     it "if reserve is followers" do
+      aa = Account.new(phone: "123")
+      aa.save
+      keyword = "followers"
+      cc = ConversationResponse.new(trigger: keyword, statement: "Howdy")
+      cc.save
+      message = Message.new(account_id: aa.id, message: keyword, direction: "out")
+      message.save
+
+      state = ConversationState.get_response(message.message)
+
+      expect(state).not_to eq("Howdy")
     end
+
     it "if reserve is focus" do
+      aa = Account.new(phone: "123")
+      aa.save
+      keyword = "focus"
+      cc = ConversationResponse.new(trigger: keyword, statement: "Howdy")
+      cc.save
+      message = Message.new(account_id: aa.id, message: keyword, direction: "out")
+      message.save
+
+      state = ConversationState.get_response(message.message)
+
+      expect(state).not_to eq("Howdy")
     end
+
   end
   describe "#get_response" do
     it "create a response" do
